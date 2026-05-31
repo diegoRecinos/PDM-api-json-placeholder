dfebd469cf83bbbfeb9e23c665ca4bd28d3795de
recinos <00182122@uca.edu.sv>
2026-05-31 12:49:38 -0600

    passed VM in screen, screen 1 and screenVM fetch and create

--- app/src/main/java/com/pdm0126/api_json_placeholder/ui/screens/screen1/Screen1.kt
+++ app/src/main/java/com/pdm0126/api_json_placeholder/ui/screens/screen1/Screen1.kt
@@ -2,35 +2,43 @@ package com.pdm0126.api_json_placeholder.ui.screens.screen1
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun Screen1(onBack: () -> Unit, modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Screen 1")
-        Spacer(modifier = Modifier.height(16.dp))
+fun Screen1(
+    viewModel: Screen1ViewModel = viewModel()
+    , onBack: () -> Unit) {
+    Column {
+        Row {
+            Button(onClick = { viewModel.fetchPosts() }) { Text("GET Posts") }
+            Button(onClick = { viewModel.createPost() }) { Text("POST (Crear)") }
+        }
 
+        if (viewModel.isLoading) {
+            CircularProgressIndicator()
+        }
 
-
-
-
-
-
-        Button(
-            onClick = onBack,
-            modifier = modifier
-        ) {Text("Back") }
+        LazyColumn {
+            items(viewModel.posts) { post ->
+                Text(text = post.title, style = MaterialTheme.typography.titleMedium)
+                Text(text = post.body)
+                HorizontalDivider()
+            }
+        }
     }
 }
--- app/src/main/java/com/pdm0126/api_json_placeholder/ui/screens/screen1/Screen1ViewModel.kt
+++ app/src/main/java/com/pdm0126/api_json_placeholder/ui/screens/screen1/Screen1ViewModel.kt
@@ -1,2 +1,48 @@
 package com.pdm0126.api_json_placeholder.ui.screens.screen1
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pdm0126.api_json_placeholder.data.model.Post
+import com.pdm0126.api_json_placeholder.repository.PostRepository.PostRepository
+import kotlinx.coroutines.launch
+
+class Screen1ViewModel(private val repository: PostRepository) : ViewModel() {
+    var posts by mutableStateOf<List<Post>>(emptyList())
+    var isLoading by mutableStateOf(false)
+
+    // Para el GET
+    fun fetchPosts() {
+        viewModelScope.launch {
+            isLoading = true
+            try {
+                posts = repository.getPosts()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                isLoading = false
+            }
+        }
+    }
+
+    // Para el POST
+    fun createPost() {
+        viewModelScope.launch {
+            try {
+                val newPost = Post(
+                    userId = 1,
+                    id = 0,
+                    title = "title Post",
+                    body = " POST body created with POST "
+                )
+                val result = repository.createPost(newPost)
+                // Agregamos el resultado al inicio de la lista para verlo en pantalla
+                posts = listOf(result) + posts
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
