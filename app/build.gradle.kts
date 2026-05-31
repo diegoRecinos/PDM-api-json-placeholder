143bffb0886c6c6b2302e8b671c430035f2e6ae3
recinos <00182122@uca.edu.sv>
2026-05-30 22:27:47 -0600

    dependencies ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,5 +76,10 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.extended)
 
     //ktor
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.logging)
 
 }
